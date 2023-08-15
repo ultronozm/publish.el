0838ba81fcd18548c0574ec1fcce18efd9710b3f
Paul Nelson <ultrono@gmail.com>
2023-08-15 21:14:32 +0200

    did it

--- publish.el
+++ publish.el
@@ -23,7 +23,30 @@
 
 ;;; Commentary:
 
-;; Convenience functions for pushing files to a git repo.
+;; Convenience functions for pushing files to a git repo.  I use this
+;; to publish files in a private git repo to a public repo linked to a
+;; web server.
+;;
+;; To use, set `publish-repo-root' to the root directory of the git
+;; repo you want to publish to.  Then, use `publish-file' to publish
+;; the current buffer, or `publish-dired-files' to publish marked
+;; files in a Dired buffer.
+;;
+;; Customize `publish-disallowed-unstaged-file-predicate' to disallow
+;; publishing if there are unstaged files in the publish repo that
+;; match a certain predicate.
+;;
+;; My use-package declaration:
+;;
+;; (defun file-is-tex-or-bib (file)
+;;   (or (string-suffix-p ".tex" file)
+;;       (string-suffix-p ".bib" file)))
+;;
+;; (use-package publish
+;;   :elpaca (:host github :repo "ultronozm/publish.el")
+;;   :custom
+;;   (publish-repo-root "~/math")
+;;   (publish-disallowed-unstaged-file-predicate #'file-is-tex-or-bib))
 
 ;;; Code:
 
@@ -31,10 +54,68 @@
 (require 'f)
 
 (defcustom publish-repo-root "~/publish"
-  "Root directory of the publish repo."
+  "Root directory of the publish repo.
+This should be the location of a valid git repository."
   :type 'directory
   :group 'publish)
 
+(defcustom publish-disallowed-unstaged-file-predicate nil
+  "Predicate to determine if file is disallowed from being unstaged."
+  :type 'function
+  :group 'publish)
+
+(defun publish--disallowed-unstaged-files-p ()
+  "Check if there are disallowed unstaged files."
+  (when publish-disallowed-unstaged-file-predicate
+    (with-temp-buffer
+      (magit-git-insert "diff" "--name-only")
+      (goto-char (point-min))
+      (let ((unstaged-files (split-string (buffer-string) "\n" t)))
+        (cl-some publish-disallowed-unstaged-file-predicate
+                 unstaged-files)))))
+
+(defun publish--core (files msg repo)
+  "Copy and commit FILES to REPO with commit message MSG."
+  ;; check if there are disallowed unstaged changes
+  (with-temp-buffer
+    (cd repo)
+    (cond
+     ((publish--disallowed-unstaged-files-p)
+      (message "Abort: There are disallowed unstaged changes in the repository."))
+     ((magit-anything-staged-p)
+      (message "Abort: There are staged changes in the repository."))
+     (t
+      (dolist (file files)
+        (let* ((filename (file-name-nondirectory file))
+               (dest-file (concat (file-name-as-directory repo) filename)))
+          (copy-file file dest-file t)
+          (magit-stage-file filename)))
+      (magit-commit-create (list "-m" msg))
+      (call-interactively 'magit-push-current-to-upstream)))))
+
+;;;###autoload
+(defun publish-file ()
+  "Copy and commit FILE to the publish repo.
+Interactively read the filename, defaulting to that of the
+current buffer."
+  (interactive)
+  (let* ((default-file (buffer-file-name))
+         (file (read-file-name "File: " default-file))
+         (filename (file-name-nondirectory file))
+         (update-p (file-exists-p (concat (file-name-as-directory publish-repo-root)
+                                          filename)))
+         (default-msg (concat (if update-p "Update" "Add") " " filename))
+         (msg (read-string "Commit message: " default-msg)))
+    (publish--core (list file) msg publish-repo-root)))
+
+;;;###autoload
+(defun publish-dired-files ()
+  "Copy and commit marked files in Dired to the publish repo."
+  (interactive
+   (let* ((files (dired-get-marked-files))
+          (default-msg "Add/update files")
+          (msg (read-string "Commit message: " default-msg)))
+     (publish--core files msg publish-repo-root))))
 
 (provide 'publish)
 ;;; publish.el ends here
